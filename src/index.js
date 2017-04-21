c525ee503f221ee6124c0356d6a1757a169eae6f
Pawel Galazka <pawel.galazka1986@gmail.com>
2017-04-21 15:08:36 +1000

    Basic implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export function Feature ({name, config, children}) {
+  const activeFeatures = Object.keys(config).filter(feature => config[feature])
+
+  let featureName = name
+  const hasNegation = name[0] === '!'
+
+  if (hasNegation) {
+    featureName = name.slice(1)
+  }
+
+  if (hasNegation && !activeFeatures.includes(featureName)) {
+    return <div>{children}</div>
+  }
+
+  if (!hasNegation && activeFeatures.includes(featureName)) {
+    return <div>{children}</div>
+  }
+
+  return null
+}
+
+Feature.propTypes = {
+  name: PropTypes.string.isRequired,
+  config: PropTypes.object.isRequired
+}
---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Feature } from './index'
+
+describe('<Feature>', () => {
+  it('should render feature content if feature active', () => {
+    const wrapper = shallow(<Feature name='test:feature' config={{'test:feature': true}}><p>content</p></Feature>)
+    expect(wrapper.html()).toEqual('<div><p>content</p></div>')
+  })
+
+  it('should not render feature content if feature not active', () => {
+    const wrapper = shallow(<Feature name='test:feature' config={{'test:feature': false}}><p>content</p></Feature>)
+    expect(wrapper.html()).toBe(null)
+
+  })
+
+  it('should not render feature content if config empty', () => {
+    const wrapper = shallow(<Feature name='test:feature' config={{}}><p>content</p></Feature>)
+    expect(wrapper.html()).toBe(null)
+  })
+})
